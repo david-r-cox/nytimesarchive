6c442aa0adab11e5300beffd5377fd493c944088
david <davidcox143@gmail.com>
2016-11-17 20:28:39 -0600

    added archive api module

---
+++ nytimesarchive.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import requests
+"""
+About:
+Python wrapper for the New York Times Archive API 
+https://developer.nytimes.com/article_search_v2.json
+"""
+
+class APIKeyException(Exception):
+    def __init__(self, message): self.message = message 
+
+class InvalidQueryException(Exception):
+    def __init__(self, message): self.message = message 
+
+class ArchiveAPI(object):
+    def __init__(self, key=None):
+        """
+        Initializes the ArchiveAPI class. Raises an exception if no API key is given.
+        :param key: New York Times API Key
+        """
+        self.key = key
+        self.root = 'http://api.nytimes.com/svc/archive/v1/{}/{}.json?api-key={}' 
+        if not self.key:
+            nyt_dev_page = 'http://developer.nytimes.com/docs/reference/keys'
+            exception_str = 'Warning: API Key required. Please visit {}'
+            raise NoAPIKeyException(exception_str.format(nyt_dev_page))
+
+    def query(self, year=None, month=None, key=None,):
+        """
+        Calls the archive API and returns the results as a dictionary.
+        :param key: Defaults to the API key used to initialize the ArchiveAPI class.
+        """
+        if not key: key = self.key
+        if (year < 1937) or not (0 < month < 13):
+            # currently the Archive API only supports year >= 1937
+            exception_str = 'Invalid query: See http://developer.nytimes.com/archive_api.json'
+            raise InvalidQueryException(exception_str)
+        url = self.root.format(year, month, key)
+        r = requests.get(url)
+        return r.json()
