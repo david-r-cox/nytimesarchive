67ca0a3db91edacef6679298720cd63235b1a2ef
davidcox143 <davidcox143@gmail.com>
2016-11-18 14:17:14 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='nytimesarchive',
+      version='0.1',
+      description='Python wrapper for the New York Times Archive API',
+      author='David Cox',
+      author_email='davidcox143@gmail.com',
+      url='https://github.com/davidcox143/nytimesarchive',
+     )
